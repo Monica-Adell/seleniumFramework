6d85d36fea348ac025d77c1c234362ecab5f3b4d
Monica-Adell <154349298+Monica-Adell@users.noreply.github.com>
2026-09-05 09:21:00 +0300

    Add Selenium automation framework

---
+++ src/test/java/selenium/taf/AppTest.java
@@ -0,0 +1,24 @@
+package selenium.taf;
+
+/*
+public class AppTest 
+    extends TestCase
+{
+
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+ static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+ 
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
+*/
